3352b4b3cb5d3bfe7fbdb6ca71d33f17efb4f5bb
gbwlxhd97 <gbwlxhd97@naver.com>
2021-08-30 22:23:59 +0900

    login

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -10,13 +10,27 @@ class Login extends Component{
         }
     }
     handleChange(event) {
+        // const {target : {value,name}} = event;
         this.setState({
-        
+            [event.target.name]: event.target.value
         })
+        // console.log(this.state);
+
     }
     render(){
+        const {id,pw} = this.state;
         return(
             <div>
+                로그인
+                <form>
+                    <input 
+                    placeholder="id입력해주세요"
+                    value={id}
+                    onChange={this.handleChange}
+                    />
+                    
+                    
+                </form>
                 
             </div>
         )
