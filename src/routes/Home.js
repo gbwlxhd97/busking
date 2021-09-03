82499e54c0786fdce52991c75c51884a77ead923
Lee-chaerin <5543705@naver.coml>
2021-09-03 22:25:41 +0900

    chaslkfa

---
+++ src/routes/Home.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Map from '../Components/Map';
+import "./style/Home.css"
+
+
+function Home() {
+
+  const x=1;
+
+  return (
+    <div>    
+   
+    <div className="header">
+      <div className="logo">
+         LOGO
+      </div>
+
+      <div className="function">
+        <ul>
+          <li className="element">1</li>
+          <li className="element">2</li>
+          <li className="element">3</li>
+          <li className="element">4</li>
+        </ul>
+      </div>
+
+      <div className="login/logout/register">
+        {x===1?(
+          <div className="login/register">
+            <a herf="">login</a>s
+            </div>
+        ):(
+          <div className="logout">
+            <p>Hello User</p>
+          </div>
+        )}
+      </div>
+    </div>
+    <div className="homeMap"><Map/></div>
+    </div>
+  );
+}
+
+
+export default Home;
