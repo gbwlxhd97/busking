3483427319cc1c7dde6a38be2ef2d83c22a0cb32
gbwlxhd97 <gbwlxhd97@naver.com>
2021-09-14 22:00:54 +0900

    노래예약 검색

--- src/api.js
+++ src/api.js
@@ -8,7 +8,7 @@ export const server = {
   createAccount: (data) =>api.post("",data), //회원가입 post Method
   loginUser: (data) =>api.post("/user",data), //로그인 ``
   getAllUser: () => api.get("/user/all"), //전체 user 데이터 받아오기  
-  searchSong: (title) => api.get(`/music/${title}`, {
+  searchSong: (title) => api.get(`/music/title/${title}`, {
     params: {
       query: encodeURIComponent(title)
     }
