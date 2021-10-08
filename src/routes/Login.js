3098e16e4762571a44a3ce8c3b1df4820b43541e
Lee-chaerin <5543705@naver.coml>
2021-10-08 19:21:27 +0900

    회원가입, 로그인 디자인

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,6 +1,50 @@
 import React,{useState} from "react";
 import { useHistory } from 'react-router-dom';
+import styled from "styled-components";
 import { server } from '../api';
+
+const Form = styled.div `
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+text-align: center;
+width: 80%
+`
+const H3 = styled.h3 `
+font-size: 1.5rem;
+font-weight: 600;
+margin-bottom: 40px;
+`
+const FormDiv = styled.div `
+margin: 20px 0;
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+const InputDiv = styled.div `
+margin: 8px 0;
+border-bottom: 2px solid #adadad;
+width: 80%
+`
+const Input = styled.input `
+padding: 8px 10px;
+width: 100%;
+border:none;
+outline:none;
+`
+const Button = styled.button `
+margin-top: 5px;
+padding: 8px 30px;
+border-radius: 10px;
+background-color: white;
+&:hover {
+    background-color: black;
+    color: white;
+}
+` 
+
+
 function Login() {
     const [values, setValues] = useState({id: "", pw: ""});
     const history = useHistory();
@@ -32,24 +76,30 @@ function Login() {
     }
     
         return(
-            <div>
-                로그인
+            <Form>
+                <H3>로그인</H3>
                 <form>
-                    <input placeholder="id입력해주세요"
-                    value={values.id || ''}
-                    type="text"
-                    name="id"
-                    onChange={handleChange}
-                    />
-                    <input placeholder="비밀번호"
-                    value={values.pw || '' }
-                    type="password"
-                    name="pw"
-                    onChange={handleChange}
-                    />
+                    <FormDiv>
+                        <InputDiv>
+                            <Input placeholder="id입력해주세요"
+                            value={values.id || ''}
+                            type="text"
+                            name="id"
+                            onChange={handleChange}
+                            />
+                        </InputDiv>
+                        <InputDiv>
+                            <Input placeholder="비밀번호"
+                            value={values.pw || '' }
+                            type="password"
+                            name="pw"
+                            onChange={handleChange}
+                            />
+                        </InputDiv>
+                    </FormDiv>
                 </form>     
-                <button onClick={handleSubmit} >로그인</button>
-            </div>
+                <Button onClick={handleSubmit} >로그인</Button>
+            </Form>
         )
     
 }
