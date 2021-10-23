5d36228db582f6f1ae47fdf465675d691edace21
gbwlxhd97 <gbwlxhd97@naver.com>
2021-10-23 16:25:24 +0900

    지도 소개글 기능 머지 & ReMap 변수명 변경

--- src/Components/ReMap.js
+++ src/Components/ReMap.js
@@ -9,7 +9,8 @@ let t1;
 let t2;
 let intro = []
 
-var data = [
+//밑에는 home.js에서 props로 각 사용자별 위치값을 [{}] 형태로 넣어주는 위치데이터
+var buskerPositions = [
     {
         imgSrc : "https://i.pinimg.com/originals/c0/da/57/c0da57e76bde0ccc9fc503bb3f77d217.jpg",
         latlng: new kakao.maps.LatLng(37.497535461505684, 127.02948149502778),
@@ -109,7 +110,7 @@ function createMarkerImage(src, size, options) {
     return markerImage;            
 }
 
-// 좌표와 마커이미지를 받아 마커를 생성하여 리턴하는 함수입니다
+// 좌표와 마커이미지를 받아 마커를 생성하여 리턴하는 함수입니다, 위치 이미지, 인트로듀스를 받아오는 함수
 function createMarker(position, image,infoData) {
     var marker = new kakao.maps.Marker({
         position: position,
@@ -123,45 +124,39 @@ function createMarker(position, image,infoData) {
 }   
 
 
-var buskerPositions = [
-    new kakao.maps.LatLng(37.497535461505684, 127.02948149502778),
-    new kakao.maps.LatLng(38.49671536281186, 127.03020491448352)
-];
 
+//버스커들 위치,이미지,소개글을 저장하는 배열상자
+let buskerDataBox = []
 
-
-// console.log(markerImageSrc.map(item => item.leader.userDetail.profileImgURL));
-console.log(tes1.map(e => e.introduce));
-let storeMarkers = []
-
-createStoreMarkers()
-function createStoreMarkers() {
-    var iwRemoveable = true; //x버튼
+createBuskerData()
+//버스커들 정보를 만들어서 각 함수(ex: 이미지,소개글 ...)에 전달해주는 함수
+function createBuskerData() {
+    let iwRemoveable = true; //x버튼
     
     for(let i=0; i <tes1.length; i++) {
-        var imageSize = new kakao.maps.Size(22, 26),
+        let imageSize = new kakao.maps.Size(22, 26),
             imageOptions = {   
                 spriteOrigin: new kakao.maps.Point(10, 36),    
                 spriteSize: new kakao.maps.Size(36, 98)  
             };       
     
-        // 마커이미지와 마커를 생성합니다0
-        var infowindow = new kakao.maps.InfoWindow({
+        let infowindow = new kakao.maps.InfoWindow({
             content: tes1[i].introduce, // 인포윈도우에 표시할 내용
-            removable : iwRemoveable
+            removable : iwRemoveable,
         });
-        var markerImage = createMarkerImage(tes1[i].profileImgURL, imageSize, imageOptions)    
-        var marker = createMarker(data[i].latlng, markerImage,infowindow);
-            storeMarkers.push(marker)
+        let markerImage = createMarkerImage(tes1[i].profileImgURL, imageSize, imageOptions)    
+        let data = createMarker(buskerPositions[i].latlng, markerImage,infowindow);
+            buskerDataBox.push(data)
         }
 }
-
-function setStoreMarkers(map) {
-    for(let i=0; i<storeMarkers.length; i++) {
-        storeMarkers[i].setMap(map)
+//최종적으로 지도에 이미지를 뿌려줄때 buskerDataBox에 있는 아이들만 보여주기위한 함수
+function setBuskerMarker(map) {
+    for(let i=0; i<buskerDataBox.length; i++) {
+        buskerDataBox[i].setMap(map)
     }   
 }
-setStoreMarkers(map) //최종적으로 지도에 이미지를 뿌려줌
+//최종적으로 지도에 이미지를 뿌려줌
+setBuskerMarker(map) 
     },[tes1])
     return (
         <div id="myMap" style={{
