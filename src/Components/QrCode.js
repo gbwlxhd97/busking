46bdb0c583c85ec553bb9561353aedbc0c096746
dongju <kdj38245@naver.com>
2021-11-10 21:13:12 +0900

    a

--- src/Components/QrCode.js
+++ src/Components/QrCode.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import styled from "styled-components";
 
+const Contents = styled.div`
+  width: 150px;
+  height: 150px;
+  position: relative;
+  top: 170px;
+  left: 35%;
+  
+`
+const Text = styled.h2`
+  color: white;
+  position: absolute;
+  top: 70%;
+  left: 5%;
+`
+const QR = styled.div`
+
+`
 var QRCode = require('qrcode.react');
 
 function QrCode() {
-	const name = '리액트';
+	const name = '버스킹 QR';
     return (
     <>
-    	<h1>{name}</h1>
-        <h2>test</h2>
-        <QRCode value="https://focused-ride-b1185f.netlify.app/" />
+      <Contents>
+        <Text>{name}</Text>
+        <QR>
+          <QRCode 
+            value="https://focused-ride-b1185f.netlify.app/"
+            includeMargin="true"
+            size="125"
+          />
+        </QR>
+      </Contents>
     </>
   );
 }
