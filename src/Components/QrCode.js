f69369bce9501af636e842e326b85aa0b771c422
gbwlxhd97 <gbwlxhd97@naver.com>
2022-05-10 21:59:59 +0900

    리드미 프로필 수정

--- src/Components/QrCode.js
+++ src/Components/QrCode.js
@@ -1,14 +1,41 @@
-import React from 'react';
 
+import React from 'react';
+import styled from "styled-components";
+
+const Contents = styled.div`
+  width: 150px;
+  height: 150px;
+  position: relative;
+  top: 170px;
+  left: 35%;
+  
+`
+const Text = styled.h2`
+  color: white;
+  position: absolute;
+  top: 70%;
+  left: 5%;
+`
+const QR = styled.div`
+`
 var QRCode = require('qrcode.react');
 
 function QrCode() {
-	const name = '리액트';
+	const name = '버스킹 QR';
     return (
     <>
-        <QRCode value="https://focused-ride-b1185f.netlify.app/" />
+      <Contents>
+        <Text>{name}</Text>
+        <QR>
+          <QRCode 
+            value="https://focused-ride-b1185f.netlify.app/"
+            includeMargin="true"
+            size="125"
+          />
+        </QR>
+      </Contents>
     </>
-);
+  );
 }
 
 export default QrCode;
